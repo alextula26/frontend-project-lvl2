1b02017823cb6e60792bcc666611e44fe24f4a4e
alextula26 <alex_marc86@mail.ru>
2020-04-19 16:08:31 +0300

    update test file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,8 +11,8 @@ test('gendiff', () => {
   - job: false
   + children: true
 }`;
-  const filepath1 = `${__dirname}/fixtures/file1.json`;
-  const filepath2 = `${__dirname}/fixtures/file2.json`;
+  const filepath1 = `${__dirname}/../__fixtures__/before.json`;
+  const filepath2 = `${__dirname}/../__fixtures__/after.json`;
   const diff = gendiff(filepath1, filepath2);
   expect(diff).toEqual(result);
 });
