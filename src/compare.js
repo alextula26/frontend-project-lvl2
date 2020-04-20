b8b7f7827172a4ec040c1b195d7fb8b3c6cbaa9b
alextula26 <alex_marc86@mail.ru>
2020-04-20 14:16:50 +0300

    abstraction for searching for file differences

--- src/compare.js
+++ src/compare.js
@@ -2,51 +2,64 @@ import _ from 'lodash';
 
 const separator = '\n';
 
-const actions = [
+const getValue = (before, after, key) => (
   {
-    action: 'update',
-    get: (data1, data2, key, acc) => {
-      if (data1[key] === data2[key]) {
-        return [...acc, ['  ', key, data1[key]]];
-      }
-      return [...acc, [' +', key, data2[key]], [' -', key, data1[key]]];
-    },
+    value: after[key], valueOld: before[key],
+  }
+);
+
+const propertyActions = [
+  {
+    state: 'added',
+    check: (before, after, key) => !_.has(before, key) && _.has(after, key),
+    getValue,
   },
   {
-    action: 'delete',
-    get: (data1, data2, key, acc) => [...acc, [' -', key, data1[key]]],
+    state: 'deleted',
+    check: (before, after, key) => _.has(before, key) && !_.has(after, key),
+    getValue,
   },
   {
-    action: 'add',
-    get: (data1, data2, key, acc) => [...acc, [' +', key, data2[key]]],
+    state: 'unchanged',
+    check: (before, after, key) => before[key] === after[key],
+    getValue,
+  },
+  {
+    state: 'changed',
+    check: (before, after, key) => before[key] !== after[key],
+    getValue,
   },
 ];
 
-const getCompareResult = (property, data1, data2, key, acc) => {
-  const object = actions.find(({ action }) => action === property);
-  return object.get(data1, data2, key, acc);
+const propertyState = {
+  added: (data) => `+ ${data.key}: ${data.value}`,
+  deleted: (data) => `- ${data.key}: ${data.valueOld}`,
+  unchanged: (data) => `  ${data.key}: ${data.value}`,
+  changed: (data) => `+ ${data.key}: ${data.value}${separator}  - ${data.key}: ${data.valueOld}`,
 };
 
-const compareResultToString = (array) => {
-  const str = array.reduce((acc, [action, key, value]) => `${acc} ${action} ${key}: ${value}${separator}`, '');
+const getDataToString = (data) => {
+  const str = data.reduce((acc, item) => {
+    const { state } = item;
+    return `${acc}  ${propertyState[state](item)}${separator}`;
+  }, '');
   return `{${separator}${str}}`;
 };
 
-const compare = (data1, data2) => {
-  const uniqKeys = _.union(_.keys(data1), _.keys(data2));
-  const result = uniqKeys.reduce((acc, key) => {
-    if (_.has(data1, key) && _.has(data2, key)) {
-      return getCompareResult('update', data1, data2, key, acc);
-    }
-
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      return getCompareResult('delete', data1, data2, key, acc);
-    }
-
-    return getCompareResult('add', data1, data2, key, acc);
-  }, []);
-
-  return compareResultToString(result);
+const getPropertyActions = (before, after, key) => {
+  // eslint-disable-next-line no-shadow
+  const { state, getValue } = propertyActions.find(({ check }) => check(before, after, key));
+  const value = getValue(before, after, key);
+  return { key, state, ...value };
 };
 
-export default compare;
+const buildData = (before, after) => {
+  const uniqKeys = _.union(_.keys(before), _.keys(after));
+  const result = uniqKeys.reduce((acc, key) => {
+    const item = getPropertyActions(before, after, key);
+    return [...acc, item];
+  }, []);
+  return getDataToString(result);
+};
+
+export default buildData;
