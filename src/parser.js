830c7732f24c1be304921b14d6975efef51cf1ed
alextula26 <alex_marc86@mail.ru>
2020-04-20 23:33:14 +0300

    refacoring parser

--- src/parser.js
+++ src/parser.js
@@ -1,15 +1,12 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default (str, format) => {
-  switch (format) {
-    case 'json':
-      return JSON.parse(str);
-    case 'yml':
-      return yaml.load(str);
-    case 'ini':
-      return ini.parse(str);
-    default:
-      return JSON.parse(str);
-  }
+export default (str, ext) => {
+  const format = {
+    json: JSON.parse,
+    yml: yaml.load,
+    ini: ini.parse,
+  };
+
+  return format[ext](str);
 };
